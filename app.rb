1412476b2f5c74c364d57556c30eec1a5f0e949c
QnYosa <55686931+QnYosa@users.noreply.github.com>
2019-10-18 10:03:57 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'pry'
 require 'bundler'
 Bundler.require
 
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/app/game.rb'
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/app/player.rb'
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/app/board.rb'
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/app/boardcase.rb'
+require_relative 'lib/app/game.rb'
+require_relative 'lib/app/player.rb'
+require_relative 'lib/app/board.rb'
+require_relative 'lib/app/boardcase.rb'
 
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/view/application.rb'
-require '/Users/dimitriyoula/Documents/THP/Jour_18/lib/view/show.rb'
+require_relative 'lib/view/application.rb'
+require_relative 'lib/view/show.rb'
 
 def perform 
     Application.new
